c4ccca55314dd0ec71a787a51b468f20092c92b9
Yasho321 <yashovardhans321@gmail.com>
2025-08-14 19:13:40 +0530

    Completed Auth Controllers And Middlewares

--- Backend/src/middlewares/auth.middlewares.js
+++ Backend/src/middlewares/auth.middlewares.js
@@ -1,1 +1,41 @@
-export const isLoggedIn = (req, res, next) => {}
+import jwt from "jsonwebtoken";
+import User from "../models/users.models.js";
+import dotenv from "dotenv";
+dotenv.config();
+export const isLoggedIn = async (req, res, next) =>{
+    try {
+        
+
+        const token = req.cookies?.token;
+
+        if(!token){
+            res.status(400).json({
+                success : false , 
+                message : "No Token Found"
+            })
+        }
+
+        const decoded = await jwt.verify(token , process.env.JWTSECRET_KEY) ;
+
+        const user = await User.findById(decoded.id).select("-password");
+
+        if(!user){
+            res.status(400).json({
+                success : false ,
+                message : "User Not Found via token"
+            })
+        }
+
+        req.user = user ; 
+
+        next();
+        
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({
+            success : false ,
+            message : "Error while authentic token",
+        })
+    }
+
+}

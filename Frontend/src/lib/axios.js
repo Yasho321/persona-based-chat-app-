659e001738ca14b0252583c0db889284b38b3ffd
Yasho321 <yashovardhans321@gmail.com>
2025-08-15 13:14:29 +0530

    Deployment Fix

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8080/api/v1',
+  baseURL: 'https://persona-based-chat-app.onrender.com/api/v1',
   withCredentials: true,
 });
